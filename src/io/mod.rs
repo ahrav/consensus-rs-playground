5a1b63f422e97c5e719b73b2567850cc6f38d1fe
AhravDutta <ahrav@amazon.com>
2025-12-11 09:52:21 -0800

    Add io-uring support for Linux platforms

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,1 +1,16 @@
+use core::ffi::c_void;
+use core::ptr::NonNull;
+use std::os::unix::io::RawFd;
+use std::time::{Duration, Instant};
+
+use io_uring::{IoUring, opcode, types};
+
+use crate::stdx::{DoublyLinkedList, ListLink, ListNode};
+
 pub mod iops;
+
+// Compile-time validations
+const _: () = assert!(
+    size_of::<usize>() >= size_of::<u32>(),
+    "Platform must have at least 32-bit addressing"
+);
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 pub mod constants;
+#[cfg(target_os = "linux")]
+pub mod io;
 pub mod stdx;
 pub mod util;
 pub mod vsr;
